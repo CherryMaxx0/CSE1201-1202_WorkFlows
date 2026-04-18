abbf2bd07dbee673e91eaae3a3db42c259382f01
cherrymaxx0 <ariksab007@gmail.com>
2026-04-18 16:02:36 +0600

    Arithmethic Operator Overloading added

--- Assignment/Assignment4.cpp
+++ Assignment/Assignment4.cpp
@@ -1,12 +1,54 @@
 #include<iostream>
 using namespace std;
 
-class Number{
+class Add{
 public:
     int a;
+    Add(){
+        cin>>a;
+    }
+    void operator+(Add yy){
+        a+=yy.a;
+    }
+};
+class Substract{
+public:
+    int a;
+    Substract(){
+        cin>>a;
+    }
+    void operator-(Substract bb){
+        a-=bb.a;
+    }
+};
+class Multiply{
+public:
+    int a;
+    Multiply(){
+        cin>>a;
+    }
+    void operator*(Multiply kk){
+        a*=kk.a;
+    }
+};
+class Divide{
+public:
+    int a;
+    Divide(){
+        cin>>a;
+    }
+    void operator/(Divide nn){
+        a/=nn.a;
+    }
 };
 
-
 int main(void){
-    
+    Add xx,yy;
+    xx+yy; cout<<"Addition: "<<xx.a<<endl;
+    Substract aa,bb;
+    aa-bb; cout<<"Substraction: "<<aa.a<<endl;
+    Multiply hh,kk;
+    hh*kk; cout<<"Multplication: "<<hh.a<<endl;
+    Divide mm,nn;
+    mm/nn; cout<<"Division: "<<mm.a<<endl;
 }
