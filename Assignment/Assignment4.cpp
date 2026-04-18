4f8e31399135a901dd6cd7c343c12f165adc7426
cherrymaxx0 <ariksab007@gmail.com>
2026-04-18 23:25:01 +0600

    = Assignment operator overloading added

--- Assignment/Assignment4.cpp
+++ Assignment/Assignment4.cpp
@@ -1,54 +1,73 @@
 #include<iostream>
 using namespace std;
 
-class Add{
-public:
-    int a;
-    Add(){
-        cin>>a;
-    }
-    void operator+(Add yy){
-        a+=yy.a;
-    }
-};
-class Substract{
-public:
-    int a;
-    Substract(){
-        cin>>a;
-    }
-    void operator-(Substract bb){
-        a-=bb.a;
-    }
-};
-class Multiply{
-public:
-    int a;
-    Multiply(){
-        cin>>a;
-    }
-    void operator*(Multiply kk){
-        a*=kk.a;
-    }
-};
-class Divide{
-public:
-    int a;
-    Divide(){
-        cin>>a;
-    }
-    void operator/(Divide nn){
-        a/=nn.a;
-    }
-};
+// //Arithmetic Operator Overloading
+// class Add{
+// public:
+//     int a;
+//     Add(){
+//         cin>>a;
+//     }
+//     void operator+(Add yy){
+//         a+=yy.a;
+//     }
+// };
+// class Substract{
+// public:
+//     int a;
+//     Substract(){
+//         cin>>a;
+//     }
+//     void operator-(Substract bb){
+//         a-=bb.a;
+//     }
+// };
+// class Multiply{
+// public:
+//     int a;
+//     Multiply(){
+//         cin>>a;
+//     }
+//     void operator*(Multiply kk){
+//         a*=kk.a;
+//     }
+// };
+// class Divide{
+// public:
+//     int a;
+//     Divide(){
+//         cin>>a;
+//     }
+//     void operator/(Divide nn){
+//         a/=nn.a;
+//     }
+// };
 
+// int main(void){
+//     Add xx,yy;
+//     xx+yy; cout<<"Addition: "<<xx.a<<endl;
+//     Substract aa,bb;
+//     aa-bb; cout<<"Substraction: "<<aa.a<<endl;
+//     Multiply hh,kk;
+//     hh*kk; cout<<"Multplication: "<<hh.a<<endl;
+//     Divide mm,nn;
+//     mm/nn; cout<<"Division: "<<mm.a<<endl;
+// }
+
+//"=" Assignment Operator Overloading
+class Assign{
+public:
+    int a;
+    Assign(){
+        cin>>a;
+    }
+    void operator=(Assign nn){
+        a=nn.a;
+    }
+};
 int main(void){
-    Add xx,yy;
-    xx+yy; cout<<"Addition: "<<xx.a<<endl;
-    Substract aa,bb;
-    aa-bb; cout<<"Substraction: "<<aa.a<<endl;
-    Multiply hh,kk;
-    hh*kk; cout<<"Multplication: "<<hh.a<<endl;
-    Divide mm,nn;
-    mm/nn; cout<<"Division: "<<mm.a<<endl;
+    Assign xx,yy;
+    cout<<"XX object then: "<<xx.a<<endl;
+    xx=yy;
+    cout<<"XX object after assigning: "<<xx.a<<endl;
 }
